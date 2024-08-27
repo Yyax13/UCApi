87d9e42ac0773559b7c486878478695560d0efb8
Yyax13 <Yyax13@proton.me>
2024-08-27 19:22:30 +0000

    sexualmente atraido por ovelhas rosas e garotas palhaço

---
+++ app/page.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+
+  const goTo = (link) => {
+    router.push(link);
+  };
+
+  return (
+    <div className="bg-zinc-800 min-h-screen flex flex-col items-center justify-center">
+      <p className="text-zinc-100 font-semibold text-9xl text-center mb-8">API Abaixo</p>
+      <button
+        className="text-cyan-700 text-lg italic text-center mb-8 mt-12 bg-cyan-100 rounded-lg p-4"
+        onClick={() => goTo("/APIGateway/consulta")}
+      >
+        aaaaaaa
+      </button>
+    </div>
+  );
+}
